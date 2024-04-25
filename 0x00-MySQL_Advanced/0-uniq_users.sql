d3801ba0bda50d6ee03bd782830c5ee27c436f41
titem-012 <titemmifta@gmail.com>
2024-04-25 18:31:27 +0000

    k

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,4 +1,4 @@
--- A SQL script that creates a table (called users) with following fields
+-- script that creates a table users with follwing fields
 -- id, email, name
 CREATE TABLE IF NOT EXISTS users (
 	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
